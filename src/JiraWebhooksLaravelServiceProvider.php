3d2527f35de788423a0941a29b801172315c08e3
Levan Velijanashvili <stichoza@gmail.com>
2023-07-24 21:47:40 +0400

    Add config publishing

--- src/JiraWebhooksLaravelServiceProvider.php
+++ src/JiraWebhooksLaravelServiceProvider.php
@@ -8,6 +8,10 @@ class JiraWebhooksLaravelServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
+        $this->publishes([
+            __DIR__ . '/../config/jira-webhooks.php' => config_path('jira-webhooks.php'),
+        ], 'jira-webhooks-config');
+
         $this->mergeConfigFrom(__DIR__ . '/../config/jira-webhooks.php', 'jira-webhooks');
     }
 }
