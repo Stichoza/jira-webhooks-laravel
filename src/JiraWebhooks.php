20b90fd1bcec19be291155cbbe46b93077698133
Levan Velijanashvili <stichoza@gmail.com>
2023-07-24 16:29:17 +0400

    Add route() method to JiraWebhooks to register a route/controller for webhook

--- src/JiraWebhooks.php
+++ src/JiraWebhooks.php
@@ -2,7 +2,13 @@
 
 namespace Stichoza\JiraWebhooksLaravel;
 
+use Illuminate\Support\Facades\Route;
+use Stichoza\JiraWebhooksLaravel\Http\Controllers\JiraWebhookController;
+
 class JiraWebhooks
 {
-
+    public static function route(string $uri = 'jira-webhook'): \Illuminate\Routing\Route
+    {
+        return Route::post($uri, JiraWebhookController::class);
+    }
 }
