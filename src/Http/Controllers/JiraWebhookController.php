4777dc0521d6b8c3a42310092409747741f38f32
Levan Velijanashvili <stichoza@gmail.com>
2023-07-24 15:58:30 +0400

    Add JiraWebhookController for handling Jira webhook events.

---
+++ src/Http/Controllers/JiraWebhookController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace Stichoza\JiraWebhooksLaravel\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Routing\Controller;
+use Illuminate\Support\Collection;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Event;
+use Illuminate\Support\Str;
+use Stichoza\JiraWebhooksData\Models\JiraWebhookData;
+
+class JiraWebhookController extends Controller
+{
+    public function __invoke(Request $request)
+    {
+        $webhook = JiraWebhookData::parse($request->all());
+
+        Collection::make(Config::get('jira-webhooks.events'))
+            ->filter(fn(string $event, string $key): bool => Str::is($key, $webhook->getWebhookEvent()))
+            ->each(fn($event) => Event::dispatch(new $event($webhook)));
+    }
+}
